402effc1f819926d00ccf48027ba803d838237d8
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 20:40:10 -0500

    prog_est_prog22
    Programa para la liquidación de varios empleados

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # prog_est_prog22
-Programa para liquidación de varios empleados
+<p>Autor: Andriks Imanol Ruiz Mártinez, Fecha: 03/03/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de  funciones, condicionales y ciclos en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables flotantes y enteros</li>
+    <li>printf para mostrar mensajes y variables</li>
+    <li>scanf</li>
+    <li>Funciones</li>
+    <li>for</li>
+    <li>Prototipos</li>
+    <li>Condicionales</li>
+    <li>El uso de include para las librerias</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Hacer un programa que calcule la liquidacion de varios empleados
+<br>
+<br>
+<p>Breve descripción:
+Es un programa que calcule la liquidacion de varios empleados usando ciclos y funciones, con estos datos:
+<ul>
+<li>Salario</li>
+<li>Antigüedad</li>
+<li>Impuestos</li>
+<li>Salarios Minimos</li>
+</ul>
+<br>
+</p>
---
+++ prog22.cpp
@@ -0,0 +1,68 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 03/03/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa de  funciones, condicionales y ciclos en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables flotantes y enteros</li>
+    -printf para mostrar mensajes y variables
+    -scanf
+    -Funciones
+    -for
+    -Prototipos
+    -Condicionales
+    -El uso de include para las librerias
+    -Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+//prototipos
+float liquidacion(float salarioQ, float ant,float isr, float salarioMin);
+//Main
+int main() {
+	//Declaración de variables
+	int empleados;
+	float liq, salario, antiguedad, impuesto, salarioMinimo;
+	//Proceso
+	printf("Introduce la cantidad de empleados: ");
+	scanf("%d",&empleados);
+	printf("Introduce el salario minimo actual: $");
+	scanf("%f",&salarioMinimo);
+	for(int i=1; i<= empleados; i++){ //Ciclo
+		printf("Empleado #%d \n", i);
+		printf("Salario quincenal: $");
+		scanf("%f",&salario);
+		printf("Antiguedad en años: ");
+		scanf("%f",&antiguedad);
+		printf("ISR: ");
+		scanf("%f",&impuesto);
+		liq= liquidacion(salario, antiguedad, impuesto, salarioMinimo);
+		//Salida
+		printf("Liquidacion neta: $%.2f\n", liq);	
+	}
+	return 0;
+}
+//Funciones
+float liquidacion(float salarioQ, float ant,float isr, float salarioMin){
+	float bonoLiquidacion, salarioP, pago20, primaAnt,liqBruto, exento, grabable, impuesto, liqNeta;
+	//Proceso
+	bonoLiquidacion=salarioQ*6; //Se le paga un bono de liquidación de 3 meses de salario
+	//salarioP= (salarioQ/15 > salarioMin*2) ? salarioQ/15 : salarioMin;
+	if (salarioQ/5 > salarioMin*2){ //Condicionales
+		salarioP=salarioQ/15;
+	}
+	else {
+		salarioP=salarioMin;
+	}
+	pago20=salarioP*20*ant; // Se le paga 20 dias de salario ponderado por cada año de antigüedad
+	primaAnt=salarioQ/15*12/ant; // Se le da una prima de antigüedad de 12 días.
+	liqBruto=bonoLiquidacion+pago20+primaAnt;
+	exento=90*salarioMin*ant; //Se le exenta la cantidad de 90 salario mínimos por cada año trabajado
+	grabable=liqBruto-exento;
+	impuesto=grabable*isr;
+	liqNeta=liqBruto-impuesto;
+	return liqNeta;
+}
+
+
